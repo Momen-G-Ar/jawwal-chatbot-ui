e2346c149cc20601e9ebfc55f9da9932fa174e61
raghad-ns <raghadnadersalem20022017@gmail.com>
2024-06-22 11:45:37 +0300

    sidebar responsiveness

--- src/Components/side-bar/SideBar.jsx
+++ src/Components/side-bar/SideBar.jsx
@@ -6,101 +6,134 @@ import "./themed-side-bar.css";
 import ChatGroup from "../ChatGroup/ChatGroup";
 
 const SideBar2 = ({
-    theme,
-    setDarkTheme,
-    handleNewChat,
-    displaySideBar,
-    setDisplaySidebar,
+  theme,
+  setDarkTheme,
+  handleNewChat,
+  displaySideBar,
+  setDisplaySidebar,
 }) => {
-    const [searchTerm, setSearchTerm] = useState("");
-    useEffect(() => {}, [searchTerm]);
-    const onChange = (e) => {
-        setSearchTerm(e.target.value);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [smallScreen, setSmallScreen] = useState(false);
+  const [displaySideBarForSmallScreen, setDisplaySideBarForSmallScreen] =
+    useState(false);
+  useState(false);
+  useState(false);
+  useEffect(() => {}, [searchTerm]);
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSmallScreen(window.innerWidth < 768);
+      //   setDisplaySidebar(window.innerWidth > 768);
     };
-    return (
-        <div
-            className={`sidebar-container ${!theme ? "light" : "dark"}`}
-            style={!displaySideBar ? { background: "none" } : {}}
-        >
-            <div className="sideHeader ">
-                <BsList
-                    size={25}
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        setDisplaySidebar(!displaySideBar);
-                    }}
-                    style={{ cursor: "pointer" }}
-                />
-                <div className="icons">
-                    <div className="uael-main-btn" data-switch-type="round_2">
-                        <div className="uael-toggle">
-                            <input
-                                className="uael-switch-round-2 elementor-clickable"
-                                type="checkbox"
-                                id="toggle_2"
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    setDarkTheme((old) => !old);
-                                }}
-                            />
-                            <label
-                                for="toggle_2"
-                                className="elementor-clickable"
-                            ></label>
-                        </div>
-                    </div>
-                    <RiChatNewLine
-                        size={22}
-                        style={{ cursor: "pointer" }}
-                        onClick={handleNewChat}
-                    />
-                </div>
+    console.log("ping...");
+    console.log("display side bar: ", displaySideBar);
+
+    // Check the initial screen size
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    // return () => {
+    //   window.removeEventListener("resize", handleResize);
+    // };
+    // eslint-disable-next-line
+  }, [window.innerWidth]);
+  return (
+    <div
+      className={`sidebar-container ${!theme ? "light" : "dark"} ${
+        !displaySideBar || (smallScreen && !displaySideBarForSmallScreen)
+          ? "hide"
+          : "show"
+      }`}
+      style={
+        !displaySideBar || (smallScreen && !displaySideBarForSmallScreen)
+          ? { background: "none" }
+          : {}
+      }
+    >
+      <div
+        className="sideHeader "
+        style={
+          !displaySideBar || (smallScreen && !displaySideBarForSmallScreen)
+            ? { borderBottom: "none" }
+            : {}
+        }
+      >
+        <BsList
+          className=""
+          size={25}
+          // style= {{minWidth: '50px'}}
+          onClick={(e) => {
+            e.stopPropagation();
+            console.log("small screen: ", smallScreen);
+            smallScreen
+              ? setDisplaySideBarForSmallScreen(!displaySideBarForSmallScreen)
+              : setDisplaySidebar(!displaySideBar);
+          }}
+          style={{ cursor: "pointer", minWidth: "40px" }}
+        />
+        <div className="icons">
+          <div className="uael-main-btn" data-switch-type="round_2">
+            <div className="uael-toggle">
+              <input
+                className="uael-switch-round-2 elementor-clickable"
+                type="checkbox"
+                id="toggle_2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setDarkTheme((old) => !old);
+                }}
+              />
+              <label for="toggle_2" className="elementor-clickable"></label>
             </div>
-            <div className="side-bar">
-                <div>
-                    <SearchBar onChange={onChange} value={searchTerm} />
-                    <div className="history">
-                        <ChatGroup
-                            searchTerm={searchTerm}
-                            groupName={"Today"}
-                            arr={[
-                                "Today chat 1",
-                                "Today chat 2",
-                                "Today chat 3",
-                            ]}
-                        />
-                        <ChatGroup
-                            searchTerm={searchTerm}
-                            groupName={"Yesterday"}
-                            arr={[
-                                "Yesterday chat 1",
-                                "Yesterday chat 2",
-                                "Yesterday chat 3",
-                            ]}
-                        />
-                        <ChatGroup
-                            searchTerm={searchTerm}
-                            groupName={"Last Week"}
-                            arr={[
-                                "Last Week chat 1",
-                                "Last Week chat 2",
-                                "Last Week chat 3",
-                            ]}
-                        />
-                        <ChatGroup
-                            searchTerm={searchTerm}
-                            groupName={"Previous"}
-                            arr={[
-                                "Previous chat 1",
-                                "Previous chat 2",
-                                "Previous chat 3",
-                            ]}
-                        />
-                    </div>
-                </div>
-            </div>
+          </div>
+          <RiChatNewLine
+            size={22}
+            style={{ cursor: "pointer" }}
+            onClick={handleNewChat}
+          />
         </div>
-    );
+      </div>
+      <div
+        className={`side-bar ${
+          !displaySideBar || (smallScreen && !displaySideBarForSmallScreen)
+            ? "hide"
+            : "show"
+        }`}
+      >
+        <div style={{ width: "100%" }}>
+          <SearchBar onChange={onChange} value={searchTerm} />
+          <div className="history">
+            <ChatGroup
+              searchTerm={searchTerm}
+              groupName={"Today"}
+              arr={["Today chat 1", "Today chat 2", "Today chat 3"]}
+            />
+            <ChatGroup
+              searchTerm={searchTerm}
+              groupName={"Yesterday"}
+              arr={["Yesterday chat 1", "Yesterday chat 2", "Yesterday chat 3"]}
+            />
+            <ChatGroup
+              searchTerm={searchTerm}
+              groupName={"Last Week"}
+              arr={["Last Week chat 1", "Last Week chat 2", "Last Week chat 3"]}
+            />
+            <ChatGroup
+              searchTerm={searchTerm}
+              groupName={"Previous"}
+              arr={["Previous chat 1", "Previous chat 2", "Previous chat 3"]}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar2;
